7b49e11a36267bdef87d870f38beaed474cc5766
Sahil <shinde1211sahil@gmail.com>
2026-01-23 14:01:18 +0530

    dd1\ commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,7 @@
 const Footer=()=>{
-
+    return(
+        <>
+        <h1>Hello This is Footer Page</h1></>
+    )
 }
 export default Footer;
--- src/pages/ViewStudent.js
+++ src/pages/ViewStudent.js
@@ -1,5 +1,42 @@
-const ViewStudent=()=>{
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+const API = "http://localhost:3000/students"
+function ViewStudent(){
+const[students, setSudents] = useState([]);
+
+const fetchStudents = async()=>{
+    const res = await axios.get(API);
+    setSudents(res.data);
+};
+
+useEffect(()=>{
+    fetchStudents();
+}, []);
+
+return(
+    <>
+    <h3>View Students</h3>
+    <table className="table table-border">
+        <thead>
+            <tr>
+                <th>Id</th><th>Name</th><th>Email</th><th>Age</th><th>Course</th>
+            </tr>
+        </thead>
+        <tbody>
+            {students.map(stu =>(
+                <tr key={stu.id}>
+                    <td>{stu.id}</td>
+                    <td>{stu.name}</td>
+                    <td>{stu.email}</td>
+                    <td>{stu.age}</td>
+                    <td>{stu.course}</td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
+    </>
+)
 
 }
 export default  ViewStudent;
